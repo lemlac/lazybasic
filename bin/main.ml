d21db2a1654abca50ee5da0f22b8bca193794b7a
Lemonator <13686726+lemlac@users.noreply.github.com>
2026-04-06 19:19:53 -0400

    Add more sum types

--- bin/main.ml
+++ bin/main.ml
@@ -1,18 +1,44 @@
 module StringMap = Map.Make(String)
 
-type values =
+type expr =
+    | EValue of value
+    | EVar of string
+    | EOp of op
+    | ECmd of cmd
+and value =
     | VWord of string
     | VString of string
+    | VNum of float
+and op =
+    | OAdd of expr * expr
+    | OSub of expr * expr
+    | OMul of expr * expr
+    | ODiv of expr * expr
+    | OMod of expr * expr
+    | OEq of expr * expr
+    | ONeq of expr * expr
+    | OLt of expr * expr
+    | OGt of expr * expr
+    | OLeq of expr * expr
+    | OGeq of expr * expr
+    | OAnd of expr * expr
+    | OOr of expr * expr
+    | ONot of expr
+and cmd =
+    | CEcho of expr
+    | CLet of string * expr
+    | CIf of expr * (expr list) * (expr list)
+    | CFor of expr * expr * expr * (expr list)
+    | CForEach of string * expr * (expr list)
+    | CWhile of expr * (expr list)
+    | CEnd
 
 let to_string tokens = 
     String.concat " " (List.map (function
         | VWord w -> "(VWord " ^ w ^ ")"
         | VString s -> "(VString \"" ^ s ^ "\")"
+        | VNum n -> "(VNum " ^ string_of_float n ^ ")"
     ) tokens)
-
-let commands = StringMap.of_list
-    [ ("echo", print_endline)
-    ]
 
 let rec process_line line tokens =
     match line with
