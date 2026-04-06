68b1d48134117c951710c12d8458dd503a3dacc6
Lemonator <13686726+lemlac@users.noreply.github.com>
2026-04-06 19:47:27 -0400

    Add to_string functions for tokens

--- bin/main.ml
+++ bin/main.ml
@@ -1,44 +1,80 @@
 module StringMap = Map.Make(String)
 
 type expr =
-    | EValue of value
-    | EVar of string
-    | EOp of op
-    | ECmd of cmd
+    | ExprValue of value
+    | ExprVar of string
+    | ExprOp of op
+    | ExprCmd of cmd
 and value =
-    | VWord of string
-    | VString of string
-    | VNum of float
+    | ValWord of string
+    | ValString of string
+    | ValNum of float
 and op =
-    | OAdd of expr * expr
-    | OSub of expr * expr
-    | OMul of expr * expr
-    | ODiv of expr * expr
-    | OMod of expr * expr
-    | OEq of expr * expr
-    | ONeq of expr * expr
-    | OLt of expr * expr
-    | OGt of expr * expr
-    | OLeq of expr * expr
-    | OGeq of expr * expr
-    | OAnd of expr * expr
-    | OOr of expr * expr
-    | ONot of expr
+    | OpAdd of expr * expr
+    | OpSub of expr * expr
+    | OpMul of expr * expr
+    | OpDiv of expr * expr
+    | OpMod of expr * expr
+    | OpEq of expr * expr
+    | OpNeq of expr * expr
+    | OpLt of expr * expr
+    | OpGt of expr * expr
+    | OpLeq of expr * expr
+    | OpGeq of expr * expr
+    | OpAnd of expr * expr
+    | OpOr of expr * expr
+    | OpNot of expr
 and cmd =
-    | CEcho of expr
-    | CLet of string * expr
-    | CIf of expr * (expr list) * (expr list)
-    | CFor of expr * expr * expr * (expr list)
-    | CForEach of string * expr * (expr list)
-    | CWhile of expr * (expr list)
-    | CEnd
+    | CmdEcho of expr
+    | CmdLet of string * expr
+    | CmdIf of expr * (expr list) * (expr list)
+    | CmdFor of string * expr * expr * (expr list)
+    | CmdForEach of string * expr * (expr list)
+    | CmdWhile of expr * (expr list)
+    | CmdBegin
+    | CmdEnd
 
-let to_string tokens = 
-    String.concat " " (List.map (function
-        | VWord w -> "(VWord " ^ w ^ ")"
-        | VString s -> "(VString \"" ^ s ^ "\")"
-        | VNum n -> "(VNum " ^ string_of_float n ^ ")"
-    ) tokens)
+let rec tokens_to_string tokens = 
+    match tokens with
+    | [] -> ""
+    | h::t -> (expr_to_string h) ^ " " ^ (tokens_to_string t)
+and expr_to_string expr =
+    match expr with
+    | ExprValue v   -> "(ExprValue " ^ (val_to_string v) ^ ")"
+    | ExprVar   var -> "(ExprVar " ^ var ^ ")"
+    | ExprOp    op  -> "(ExprOp " ^ (op_to_string op) ^ ")"
+    | ExprCmd   cmd -> "(ExprCmd " ^ (cmd_to_string cmd) ^ ")"
+and val_to_string v =
+    match v with
+    | ValWord   w -> "(ValWord '" ^ w ^ "')"
+    | ValString s -> "(ValString \"" ^ s ^ "\")"
+    | ValNum    n -> "(ValNum " ^ (string_of_float n) ^ ")"
+and op_to_string op =
+    match op with
+    | OpAdd (e1, e2) -> "(OpAdd " ^ (expr_to_string e1) ^ " " ^ (expr_to_string e2) ^ ")"
+    | OpSub (e1, e2) -> "(OpSub " ^ (expr_to_string e1) ^ " " ^ (expr_to_string e2) ^ ")"
+    | OpMul (e1, e2) -> "(OpMul " ^ (expr_to_string e1) ^ " " ^ (expr_to_string e2) ^ ")"
+    | OpDiv (e1, e2) -> "(OpDiv " ^ (expr_to_string e1) ^ " " ^ (expr_to_string e2) ^ ")"
+    | OpMod (e1, e2) -> "(OpMod " ^ (expr_to_string e1) ^ " " ^ (expr_to_string e2) ^ ")"
+    | OpEq  (e1, e2) -> "(OpEq "  ^ (expr_to_string e1) ^ " " ^ (expr_to_string e2) ^ ")"
+    | OpNeq (e1, e2) -> "(OpNeq " ^ (expr_to_string e1) ^ " " ^ (expr_to_string e2) ^ ")"
+    | OpLt  (e1, e2) -> "(OpLt "  ^ (expr_to_string e1) ^ " " ^ (expr_to_string e2) ^ ")"
+    | OpGt  (e1, e2) -> "(OpGt "  ^ (expr_to_string e1) ^ " " ^ (expr_to_string e2) ^ ")"
+    | OpLeq (e1, e2) -> "(OpLeq " ^ (expr_to_string e1) ^ " " ^ (expr_to_string e2) ^ ")"
+    | OpGeq (e1, e2) -> "(OpGeq " ^ (expr_to_string e1) ^ " " ^ (expr_to_string e2) ^ ")"
+    | OpAnd (e1, e2) -> "(OpAnd " ^ (expr_to_string e1) ^ " " ^ (expr_to_string e2) ^ ")"
+    | OpOr  (e1, e2) -> "(OpOr "  ^ (expr_to_string e1) ^ " " ^ (expr_to_string e2) ^ ")"
+    | OpNot ( e    ) -> "(OpNot " ^ (expr_to_string e ) ^ ")"
+and cmd_to_string cmd =
+    match cmd with
+    | CmdEcho    (e) -> "(CmdEcho " ^ (expr_to_string e) ^ ")"
+    | CmdLet     (v, e) -> "(CmdLet " ^ v ^ " " ^ (expr_to_string e) ^ ")"
+    | CmdIf      (cond, then_branch, else_branch) -> "(CmdIf " ^ (expr_to_string cond) ^ " " ^ (tokens_to_string then_branch) ^ " " ^ (tokens_to_string else_branch) ^ ")"
+    | CmdFor     (var, start, stop, body) -> "(CmdFor " ^ var ^ " " ^ (expr_to_string start) ^ " " ^ (expr_to_string stop) ^ " " ^ (tokens_to_string body) ^ ")"
+    | CmdForEach (var, list, body) -> "(CmdForEach " ^ var ^ " " ^ (expr_to_string list) ^ " " ^ (tokens_to_string body) ^ ")"
+    | CmdWhile   (cond, body) -> "(CmdWhile " ^ (expr_to_string cond) ^ " " ^ (tokens_to_string body) ^ ")"
+    | CmdBegin -> "CmdBegin"
+    | CmdEnd   -> "CmdEnd"
 
 let rec process_line line tokens =
     match line with
@@ -57,7 +93,7 @@ and process_word word line tokens =
         | 'a' .. 'z'
         | 'A' .. 'Z'
         | '0' .. '9' -> process_word (word ^ (String.make 1 h)) t tokens
-        | _ -> process_line t (tokens @ [VWord word])
+        | _ -> process_line t (tokens @ [ExprValue (ValWord word)])
 and process_string quote str line tokens =
     match line with
     | [] -> tokens
@@ -67,7 +103,7 @@ and process_string quote str line tokens =
             | [] -> tokens
             | h::t -> process_string quote (str ^ (String.make 1 h)) t tokens
         else if h == quote
-        then process_line t (tokens @ [VString str])
+        then process_line t (tokens @ [ExprValue (ValString str)])
         else process_string quote (str ^ (String.make 1 h)) t tokens
 
 let process_file filename =
@@ -86,7 +122,7 @@ let process_file filename =
     | e ->
         close_in_noerr ic;
         raise e);
-    print_endline (to_string !tokens)
+    print_endline (tokens_to_string !tokens)
 
 let () = process_file Sys.argv.(1)
 
