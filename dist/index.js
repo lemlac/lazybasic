d977c05839eda396473a6cf82423166eadcb1c85
Lemoantor32 <13686726+lemlac@users.noreply.github.com>
2026-06-11 15:28:45 -0400

    Set up project

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.greet = greet;
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+const message = greet("World");
+console.log(message);
+//# sourceMappingURL=index.js.map
