dc6dc672da420ec7f8f3714c6a9f7b2c22ac368c
Lemoantor32 <13686726+lemlac@users.noreply.github.com>
2026-06-11 16:27:45 -0400

    Make tokenizer

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,92 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.greet = greet;
-function greet(name) {
-    return `Hello, ${name}!`;
+exports.tokenize = tokenize;
+var TokenTag;
+(function (TokenTag) {
+    TokenTag[TokenTag["SYMBOL"] = 0] = "SYMBOL";
+    TokenTag[TokenTag["WORD"] = 1] = "WORD";
+    TokenTag[TokenTag["NUMBER_LITERAL"] = 2] = "NUMBER_LITERAL";
+    TokenTag[TokenTag["STRING_LITERAL"] = 3] = "STRING_LITERAL";
+    TokenTag[TokenTag["FUNCTION"] = 4] = "FUNCTION";
+    TokenTag[TokenTag["END"] = 5] = "END";
+    TokenTag[TokenTag["RETURN"] = 6] = "RETURN";
+    TokenTag[TokenTag["B_START"] = 7] = "B_START";
+    TokenTag[TokenTag["B_END"] = 8] = "B_END";
+    TokenTag[TokenTag["LINE_BREAK"] = 9] = "LINE_BREAK";
+})(TokenTag || (TokenTag = {}));
+function tokenize(input) {
+    let tokens = [];
+    let chunk = '';
+    let isComment = false;
+    let token = null;
+    for (let c of input) {
+        if (isComment) {
+            if (c === '\n') {
+                isComment = false;
+                tokens.push({ tag: TokenTag.LINE_BREAK });
+            }
+            continue;
+        }
+        if (c === "'") {
+            isComment = true;
+            if (token != null) {
+                switch (token.tag) {
+                    case TokenTag.WORD:
+                        token.word = chunk;
+                        break;
+                    case TokenTag.SYMBOL:
+                        token.symbol = chunk;
+                }
+                tokens.push(token);
+                chunk = '';
+                token = null;
+            }
+            continue;
+        }
+        if (token != null) {
+            switch (token.tag) {
+                case TokenTag.WORD:
+                    if (isAlphanumeric(c)) {
+                        chunk += c;
+                        continue;
+                    }
+                    token.word = chunk;
+                    break;
+                case TokenTag.SYMBOL:
+                    if (isSymbol(c)) {
+                        chunk += c;
+                        continue;
+                    }
+                    token.symbol = chunk;
+            }
+            tokens.push(token);
+            chunk = '';
+            token = null;
+        }
+        if (isAlphanumeric(c)) {
+            token = { tag: TokenTag.WORD };
+            chunk = c;
+        }
+        else if (isSymbol(c)) {
+            token = { tag: TokenTag.SYMBOL };
+            chunk = c;
+        }
+        else if (c === '\n') {
+            tokens.push({ tag: TokenTag.LINE_BREAK });
+        }
+        else if (!isWhitespace(c)) {
+            throw new Error(`Unexpected token: ${c}`);
+        }
+    }
+    return tokens;
 }
-const message = greet("World");
-console.log(message);
+function isWhitespace(s) {
+    return /\s/.test(s);
+}
+function isAlphanumeric(s) {
+    return /\w/.test(s);
+}
+function isSymbol(s) {
+    return /[~!@#$%^&*-+=\\|,<.>/?]/.test(s);
+}
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,97 @@
-export function greet(name: string): string {
-    return `Hello, ${name}!`;
+enum TokenTag {
+    SYMBOL,
+    WORD,
+    NUMBER_LITERAL,
+    STRING_LITERAL,
+    FUNCTION,
+    END,
+    RETURN,
+    B_START,
+    B_END,
+    LINE_BREAK,
 }
 
-const message: string = greet("World");
-console.log(message);
+type Token = 
+    | { tag: TokenTag.SYMBOL, symbol?: string }
+    | { tag: TokenTag.WORD, word?: string }
+    | { tag: TokenTag.NUMBER_LITERAL, number?: number }
+    | { tag: TokenTag.STRING_LITERAL, string?: string }
+    | { tag: TokenTag.B_START, bracketStart?: '('|'['|'{' }
+    | { tag: TokenTag.B_END, bracketEnd?: ')'|']'|'}' }
+    | { tag: TokenTag.FUNCTION | TokenTag.END | TokenTag.RETURN | TokenTag.LINE_BREAK }
+
+export function tokenize(input: string): Token[] {
+    let tokens: Token[] = [];
+    let chunk = '';
+    let isComment = false;
+    let token: Token | null = null;
+    for (let c of input) {
+        if (isComment) {
+            if (c === '\n') {
+                isComment = false;
+                tokens.push({ tag: TokenTag.LINE_BREAK });
+            }
+            continue;
+        }
+        if (c === "'") {
+            isComment = true;
+            if (token != null) {
+                switch (token.tag) {
+                    case TokenTag.WORD:
+                        token.word = chunk;
+                        break;
+                    case TokenTag.SYMBOL:
+                        token.symbol = chunk;
+                }
+                tokens.push(token);
+                chunk = '';
+                token = null;
+            }
+            continue;
+        }
+        if (token != null) {
+            switch (token.tag) {
+                case TokenTag.WORD:
+                    if (isAlphanumeric(c)) {
+                        chunk += c;
+                        continue;
+                    }
+                    token.word = chunk;
+                    break;
+                case TokenTag.SYMBOL:
+                    if (isSymbol(c)) {
+                        chunk += c;
+                        continue;
+                    }
+                    token.symbol = chunk;
+            }
+            tokens.push(token);
+            chunk = '';
+            token = null;
+        }
+        if (isAlphanumeric(c)) {
+            token = { tag: TokenTag.WORD };
+            chunk = c;
+        } else if (isSymbol(c)) {
+            token = { tag: TokenTag.SYMBOL };
+            chunk = c;
+        } else if (c === '\n') {
+            tokens.push({ tag: TokenTag.LINE_BREAK });
+        } else if (!isWhitespace(c)) {
+            throw new Error(`Unexpected token: ${c}`);
+        }
+    }
+    return tokens;
+}
+
+function isWhitespace(s: string): boolean {
+    return /\s/.test(s);
+}
+
+function isAlphanumeric(s: string): boolean {
+    return /\w/.test(s);
+}
+
+function isSymbol(s: string): boolean {
+    return /[~!@#$%^&*-+=\\|,<.>/?]/.test(s);
+}
